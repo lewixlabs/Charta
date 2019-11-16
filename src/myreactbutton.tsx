9a7f194f3cb2f6948eeb5c8c8080e2f7ef495f6a
Luigino De Togni <lewixlabs@gmail.com>
2019-11-16 23:13:33 +0100

    test modules removed

--- src/myreactbutton.tsx
+++ src/myreactbutton.tsx
@@ -1,12 +1,9 @@
 import React = require("react");
-import { tsPlay } from "./funny";
-
 
 export class MyReactButton extends React.Component {
 
     playTsCard() {
         console.log("wow!!!!")
-        tsPlay();
     }
 
     render() {
