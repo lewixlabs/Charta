8a45533a3c71ddd35ff12da04c63684c1f71feb9
lewix <>
2019-11-22 16:41:40 +0100

    work in progress :)

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,8 @@ function createWindow() {
   });
 }
 
+require("electron-reload")(__dirname);
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
--- src/react-engine/ReaderPanel.tsx
+++ src/react-engine/ReaderPanel.tsx
@@ -8,9 +8,9 @@ export class ReaderPanel extends React.Component<IReaderProps> {
 
     public render() {
         return (
-            <header className="toolbar toolbar-header">
-                <h1 className="title">Reader: {this.props.name}</h1>
-            </header>
+                <div>
+                    Model: {this.props.name}
+                </div>
         );
     }
 }
