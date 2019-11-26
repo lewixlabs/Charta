290c1054c48298567a15eee95729ca85308be5c3
lewix <>
2019-11-26 16:08:04 +0100

    fix props propagation https://it.reactjs.org/docs/react-component.html#constructor :)

--- src/react-engine/CardPanel.tsx
+++ src/react-engine/CardPanel.tsx
@@ -7,30 +7,30 @@ export class CardPanel extends React.Component<ICardManagerStatus, ICardInfo> {
     constructor(props: ICardManagerStatus) {
         super(props);
 
-        this.state = {
-            atr: props.isCardInserted ? props.cardInfo.atr : "",
-            cardType: props.isCardInserted ? props.cardInfo.cardType : "",
-        };
+        // this.state = {
+        //     atr: props.isCardInserted ? props.cardInfo.atr : "",
+        //     cardType: props.isCardInserted ? props.cardInfo.cardType : "",
+        // };
     }
 
     // tslint:disable-next-line: max-line-length
     // https://developmentarc.gitbooks.io/react-indepth/content/life_cycle/update/postrender_with_componentdidupdate.html
     // https://it.reactjs.org/docs/react-component.html#componentdidupdate
-    public componentDidUpdate(prevProps: ICardManagerStatus, prevState: {}) {
-        // tslint:disable-next-line: max-line-length
-        if (prevProps !== this.props) {
-            this.setState({
-                atr: this.props.isCardInserted ? this.props.cardInfo.atr : "",
-                cardType: this.props.isCardInserted ? this.props.cardInfo.cardType : "",
-            });
-        }
-    }
+    // public componentDidUpdate(prevProps: ICardManagerStatus, prevState: {}) {
+    //     // tslint:disable-next-line: max-line-length
+    //     if (prevProps !== this.props) {
+    //         this.setState({
+    //             atr: this.props.isCardInserted ? this.props.cardInfo.atr : "",
+    //             cardType: this.props.isCardInserted ? this.props.cardInfo.cardType : "",
+    //         });
+    //     }
+    // }
 
     public render() {
         return (
             <div>
-                <CustomBox title="Atr" text={this.state.atr}/>
-                <CustomBox title="Card Type" text={this.state.cardType}/>
+                <CustomBox title="Atr" text={this.props.isCardInserted ? this.props.cardInfo.atr : ""}/>
+                <CustomBox title="Card Type" text={this.props.isCardInserted ? this.props.cardInfo.cardType : ""}/>
             </div>
         );
     }
