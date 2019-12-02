ef51c99335d6579c5695ed7cf2b5de992fa9b387
lewix <>
2019-12-02 17:34:39 +0100

    UI improvements

--- src/react-engine/CardPanel.tsx
+++ src/react-engine/CardPanel.tsx
@@ -1,36 +1,15 @@
 import React from "react";
 import { ICardInfo, ICardManagerStatus } from "../smartcard/cardmanager";
 import { CustomBox } from "./chartaui/CustomBox";
+import { CustomTextField } from "./chartaui/CustomTextField";
 
 export class CardPanel extends React.Component<ICardManagerStatus, ICardInfo> {
 
-    constructor(props: ICardManagerStatus) {
-        super(props);
-
-        // this.state = {
-        //     atr: props.isCardInserted ? props.cardInfo.atr : "",
-        //     cardType: props.isCardInserted ? props.cardInfo.cardType : "",
-        // };
-    }
-
-    // tslint:disable-next-line: max-line-length
-    // https://developmentarc.gitbooks.io/react-indepth/content/life_cycle/update/postrender_with_componentdidupdate.html
-    // https://it.reactjs.org/docs/react-component.html#componentdidupdate
-    // public componentDidUpdate(prevProps: ICardManagerStatus, prevState: {}) {
-    //     // tslint:disable-next-line: max-line-length
-    //     if (prevProps !== this.props) {
-    //         this.setState({
-    //             atr: this.props.isCardInserted ? this.props.cardInfo.atr : "",
-    //             cardType: this.props.isCardInserted ? this.props.cardInfo.cardType : "",
-    //         });
-    //     }
-    // }
-
     public render() {
         return (
-            <div>
-                <CustomBox title="Atr" text={this.props.isCardInserted ? this.props.cardInfo.atr : ""}/>
+            <div hidden={!this.props.isCardInserted}>
                 <CustomBox title="Card Type" text={this.props.isCardInserted ? this.props.cardInfo.cardType : ""}/>
+                <CustomTextField label="ATR" readOnly={true} text={this.props.isCardInserted ? this.props.cardInfo.atr.toUpperCase() : ""} charsLength={30}/>
             </div>
         );
     }
